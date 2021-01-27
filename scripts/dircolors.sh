22a0556281a749f39163b6fa8ea8d6ef2897fa44
Grimm-Child <GrimmStar.Graphics@gmail.com>
2021-01-27 02:39:29 -0500

    Create dircolors.sh

---
+++ scripts/dircolors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# DIRCOLORS
+# Create ~/.dircolors and set the color of directories
+echo "DIR 04;34 # directory
+OTHER_WRITABLE 04;36 # dir that is other-writable (o+w) and not sticky
+STICKY 04;44 # dir with the sticky bit set (+t) and not other-writable
+STICKY_OTHER_WRITABLE 04;42 # dir that is sticky and other-writable (+t,o+w)" > ~/.dircolors
